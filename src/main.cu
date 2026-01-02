9927d8a634aa7d99b12b23a8a5d7d9979334c786
FreePlacki <bieleckipawel10@gmail.com>
2026-01-02 22:37:00 +0100

    Add glfw

--- src/main.cu
+++ src/main.cu
@@ -1,4 +1,5 @@
 #include "assert.h"
+#include <GLFW/glfw3.h>
 #include <cuda_runtime.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -28,6 +29,38 @@ int main(int argc, char **argv) {
     char *pname = argv[0];
     if (argc != 1)
         usage(pname);
+
+    if (!glfwInit()) {
+        printf("GLFW init failed");
+        exit(1);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window =
+        glfwCreateWindow(800, 600, "Fishies", NULL, NULL);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create window\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // vsync
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
